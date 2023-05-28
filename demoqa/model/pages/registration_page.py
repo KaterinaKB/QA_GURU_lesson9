82b243111ac74373452cca6cf775288f555d953f
Voronova Ekaterina <e.b.kochekova@gmail.com>
2023-05-28 11:58:30 +0300

    add telegram-bot

--- demoqa/model/pages/registration_page.py
+++ demoqa/model/pages/registration_page.py
@@ -22,7 +22,7 @@ class RegistrationPage:
     def open(self):
         browser.open("/automation-practice-form")
         browser.all("[id^=google_ads][id$=container__]").with_(timeout=10).wait_until(
-            have.size_greater_than_or_equal(2)
+            have.size_greater_than_or_equal(3)
         )
         browser.all("[id^=google_ads][id$=container__]").perform(command.js.remove)
 
